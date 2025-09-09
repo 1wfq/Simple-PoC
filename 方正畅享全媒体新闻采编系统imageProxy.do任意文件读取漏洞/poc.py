01ac8a0d500ded3d0fda2b9fb415daa72d46e7b0
1wfq <3032824435@qq.com>
2025-09-09 21:43:59 +0800

    Update poc.py

--- 方正畅享全媒体新闻采编系统imageProxy.do任意文件读取漏洞/poc.py
+++ 方正畅享全媒体新闻采编系统imageProxy.do任意文件读取漏洞/poc.py
@@ -39,7 +39,7 @@ def poc(target):
 def banner():
     pass
 def main():
-    parse=argparse.ArgumentParser(description="SpringBlade api/blade-system/menu/list接口存在SQL注入")
+    parse=argparse.ArgumentParser(description="方正畅享全媒体新闻采编系统imageProxy.do任意文件读取漏洞")
     parse.add_argument('-u','--url',dest='url',type=str,help='please input your link')
     parse.add_argument('-f','--file',dest='file',type=str,help='please input your file path')
     args=parse.parse_args()
